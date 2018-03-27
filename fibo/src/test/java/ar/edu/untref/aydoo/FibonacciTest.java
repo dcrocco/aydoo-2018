8681729c5144f4dd0e99f702f11c4c3e9c53308a
Daniel Crocco <dcrocco@openpyme.com.ar>
2018-03-27 00:15:26 -0300

    [ADD] Ejercicio de Fibonacci

---
+++ fibo/src/test/java/ar/edu/untref/aydoo/FibonacciTest.java
@@ -0,0 +1,39 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class FibonacciTest 
+{
+    @Test
+    public void testFirstNumber()
+    {
+        FibonacciCalculator fibo = new FibonacciCalculator();
+        int result = fibo.get_fibonacci_number(1);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void testSecondNumber()
+    {
+        FibonacciCalculator fibo = new FibonacciCalculator();
+        int result = fibo.get_fibonacci_number(2);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void testFifthNumber()
+    {
+        FibonacciCalculator fibo = new FibonacciCalculator();
+        int result = fibo.get_fibonacci_number(5);
+        Assert.assertEquals(5, result);
+    }
+
+    @Test
+    public void testInvalidNumber()
+    {
+        FibonacciCalculator fibo = new FibonacciCalculator();
+        int result = fibo.get_fibonacci_number(-5);
+        Assert.assertEquals(-1, result);
+    }
+}
